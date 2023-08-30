cad23ec336ff4712e0afb0d8323a76312a552f8c
Thomy Ledesma <thoomiledesma@gmail.com>
2023-08-30 11:45:35 -0300

    first five exercises

---
+++ exercises/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+def sumatoria_sum(n: int) -> int:
+    return sum(range(1, n+1))
+
+
+# NO MODIFICAR - INICIO
+assert sumatoria_sum(1) == 1
+assert sumatoria_sum(100) == 5050
+# NO MODIFICAR - FIN
